19a00bc401dc1abe9adf10d737001a1f73208093
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-01-08 22:37:58 +0530

    Testing new blobs | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ lunch aospa_lavender-user
 export TZ=Asia/Kolkata
 
 compile_plox () {
-ls /tmp/rom/out/target/product/lavender/system.img || make systemimage -j10
-ls /tmp/rom/out/target/product/lavender/system.img && ./rom-build.sh lavender -t user -s keys -j10
+./rom-build.sh lavender -t user -s keys -j10
 ls /tmp/rom/aospa*.zip && mv -f /tmp/rom/aospa*.zip /tmp/rom/out/target/product/lavender
 }
