4bcad83b480c33e9723b0992175e3d2de33f9571
ImSpiDy <SpiDy2713@gmail.com>
2023-01-13 19:45:57 +0000

    make bliss plox
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 # Normal build steps
 . build/envsetup.sh
-lunch aosp_lavender-userdebug
+lunch bliss_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 
 compile_plox () {
-mke bacon -j10
+blissify -v lavender -j10
 }
